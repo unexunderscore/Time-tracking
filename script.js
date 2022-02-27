0dbdb6d7f6860cf33afcc8501d2024843cfa4a92
Attila Tóth <toth_attila@outlook.hu>
2022-02-27 17:59:14 +0100

    Load in data.json file

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // Checking the connection between js and html 
 const greetings = "Hello there!"
 console.log(greetings);
+
+// Loading in json data
+fetch("./data.json")
+.then(response => {
+   return response.json();
+})
+.then(jsondata => console.log(jsondata));
